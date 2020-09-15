04ab4fa7bb8c36f0b6dd9f441dcdec68ec535cdf
Steve <sshoemaker@smu.edu>
2020-09-15 16:17:37 -0500

    Added node api-server and postgres
    Added skeleton node server, postgres, and dockerfiles for them

---
+++ api-server/index.js
@@ -0,0 +1,35 @@
+//CRM Application Index
+//Entry point for application
+
+//Express App that handles requests
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+
+//Uses Rama Node Logger
+//MIT Liscence
+const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+
+app.use(cors({
+    credentials:true
+  }));
+
+const logger = log({ label: 'user-service' , console: true, file: true });
+
+
+app.get("/",function(req,res){
+    res.status(200).send("Cry With Me API is up and running");
+    
+});
+
+
+app.listen(443, () => {
+    logger.info(`Listening to requests on 443`);
+});
