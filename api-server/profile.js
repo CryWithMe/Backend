f1ba012e1854419879aee2f86a4ff6b96dcf1ee7
sshoemaker@smu.edu <sshoemaker@smu.edu>
2020-11-22 13:19:57 -0600

    get condition

--- api-server/profile.js
+++ api-server/profile.js
@@ -35,6 +35,43 @@ exports.init = (app) => {
         }
     })
 
+    app.get("/condition/:username", (req,res) => {
+        if(req.params.username){
+            pool.connect((err,client,release)=> {
+                if(err){
+                    res.sendStatus(500);
+                } else {
+                    client.query(
+                        `SELECT 
+                            condition.condition
+                        FROM
+                            condition
+                        JOIN
+                            account
+                        ON
+                            account.id=condition.id
+                        WHERE
+                            account.username = $1
+                        ORDER BY
+                            condition.lastupdatedate
+                        DESC LIMIT 1;`,
+                        [req.body.username],
+                        (err,rows)=>{
+                            if(err){
+                                res.sendStatus(500)
+                            }else {
+                                res.status(200).send(rows);
+                            }
+                        })
+                }
+            })
+        } else {
+            res.sendStatus(400);
+        }
+
+
+    })
+
 
 
 
