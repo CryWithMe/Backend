6b7f32c69bdd4c50e4a48d28abc14d089288b116
sshoemaker@smu.edu <sshoemaker@smu.edu>
2020-11-21 20:26:22 -0600

    added something to token

--- api-server/pushToken.js
+++ api-server/pushToken.js
@@ -3,12 +3,12 @@ const {v4} = require("uuid");
 exports.init = (app) => {
 
     app.post("/token", (req,res)=>{
-        if(req.body.id && req.body.token){
+        if(req.body.accountId && req.body.token){
             pool.connect( (err,client,release) =>{
                 if(err){
                     res.sendStatus(500);
                 } else {
-                    client.query("INSERT INTO push(id,pushtoken,active,lastupdatedate) VALUES($1,$2, true, current_timestamp);", [req.body.id && req.body.token], (err,result)=>{
+                    client.query("INSERT INTO push(id,pushtoken,active,lastupdatedate) VALUES($1,$2, true, current_timestamp);", [req.body.accountId && req.body.token], (err,result)=>{
                         if(err){
                             res.sendStatus(500)
                         }else {
